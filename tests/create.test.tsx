3c57366118c74df9320a085102d8ff479c018854
Artenaum <artenaum@gmail.com>
2026-01-14 15:55:17 +0300

    Clean up create.test.tsx, added test for fetchTracks action

--- tests/create.test.tsx
+++ tests/create.test.tsx
@@ -15,25 +15,16 @@ jest.mock('next/router', () => ({
 
 jest.mock('../components/Player', () => ({children}) => <div>{children}</div>)
 
-window.HTMLMediaElement.prototype.load = () => { /* do nothing */ };
-window.HTMLMediaElement.prototype.play = () => { return Promise.resolve(); };
-window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-
-
 test('Ensure that create.tsx has 3 <TextField> components and that they have correct labels', () => {
 	const MockProvider = Provider as any
 
-
-	render(
+	const create = render(
 		<MockProvider store={store}>
 			<Create/>
 		</MockProvider>
 	)
 
-	const textFields = screen.getAllByTestId('create-track-form-field')
-	expect(textFields).toHaveLength(3)
-
-	expect(textFields[0]).toHaveTextContent("Название трека")
-	expect(textFields[1]).toHaveTextContent("Имя исполнителя")
-	expect(textFields[2]).toHaveTextContent("Слова к треку")
+	expect(create.container).toHaveTextContent("Название трека")
+	expect(create.container).toHaveTextContent("Имя исполнителя")
+	expect(create.container).toHaveTextContent("Слова к треку")
 })
---
+++ tests/fetchTracks.test.ts
@@ -0,0 +1,48 @@
+import thunk from 'redux-thunk'
+import configureMockStore from 'redux-mock-store'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { fetchTracks } from '../store/actions-creators/track'
+import { TrackActionTypes } from '../types/track'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+const mockAxios = new MockAdapter(axios)
+
+describe('test fetchTracks() action', () => {
+	afterEach(() => {
+		mockAxios.reset()
+	})
+
+	it('get FETCH_TRACKS on success', async () => {
+		const mockTracks = [{id: 1, name: 'Track 1'}]
+
+		mockAxios.onGet('http://localhost:5000/tracks').reply(200, mockTracks)
+
+		const store = mockStore({tracks: []})
+
+		await store.dispatch<any>(fetchTracks())
+
+		const actions = store.getActions()
+
+		expect(actions[0]).toEqual({
+			type: TrackActionTypes.FETCH_TRACKS,
+			payload: mockTracks
+		})
+	})
+
+	it('get FETCH_TRACKS_ERROR on failure', async () => {
+		mockAxios.onGet('http://localhost:5000/tracks').reply(500)
+
+		const store = mockStore({tracks: []})
+
+		await store.dispatch<any>(fetchTracks())
+
+		const actions = store.getActions()
+
+		expect(actions[0]).toEqual({
+			type: TrackActionTypes.FETCH_TRACKS_ERROR,
+			payload: 'Произошла ошибка при загрузке треков'
+		})
+	})
+})
